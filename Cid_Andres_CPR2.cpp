c93930d868b291d35757498a2b2ed3308c976c5f
Yuume Kaoo <68014278+YuumeKaoo@users.noreply.github.com>
2024-01-04 13:58:35 +0800

    Add files via upload

---
+++ Cid_Andres_CPR2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+string fname;
+string program;
+int tc1;
+int tc2;
+int tc3;
+int tc4;
+int lore1;
+int lore2;
+int lore3;
+int lore4;
+int cd1;
+int cd2;
+int cd3;
+int cd4;
+int music1;
+int music2;
+int music3;
+int music4;
+
+
+//Subjects: Theorycrafting, Lore, Character Design, Music
+
+int main(){
+	
+	system("Color 0A");
+	
+	cout << "*********************************************************************" << endl;
+	cout << "\t\t  HoYoverse Institute of Gacha Games" << endl;
+	cout << "\t\t\tGrades Encoding System" << endl;
+	cout << "*********************************************************************" << endl;
+	
+	cout << "Student Name: \t";
+	cin.ignore();
+    getline(cin, fname);
+	
+	cout << "Program Name: \t";
+	cin.ignore();
+    getline(cin, program);
+	
+	
+	
+	return 0;
+}
