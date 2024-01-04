3d464d0f306d120ae8364dfd090484ab6288341b
JamVSCode <143859225+JamVSCode@users.noreply.github.com>
2024-01-04 15:45:27 +0800

    Update Cid_Andres_CPR2.cpp

--- Cid_Andres_CPR2.cpp
+++ Cid_Andres_CPR2.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <windows.h>
+#include <conio.h>
+#include <iomanip>
+#include <ios>
 
 using namespace std;
 
@@ -10,22 +13,26 @@ string course1;
 string course2;
 string course3;
 string course4;
-int subjA1;
-int subjA2;
-int subjA3;
-int subjA4;
-int subjB1;
-int subjB2;
-int subjB3;
-int subjB4;
-int subjC1;
-int subjC2;
-int subjC3;
-int subjC4;
-int subjD1;
-int subjD2;
-int subjD3;
-int subjD4;
+float avg1;
+float avg2;
+float avg3;
+float avg4;
+float subjA1;
+float subjA2;
+float subjA3;
+float subjA4;
+float subjB1;
+float subjB2;
+float subjB3;
+float subjB4;
+float subjC1;
+float subjC2;
+float subjC3;
+float subjC4;
+float subjD1;
+float subjD2;
+float subjD3;
+float subjD4;
 
 //Subjects: Theorycrafting, Lore, Character Design, Music
 
@@ -40,19 +47,16 @@ int main(){
 	
 	StudentName:
 	cout << "Student Name: \t";
-	cin.ignore();
     getline(cin, fname);
 	
 	ProgramName:
-	cout << "Program Name: \t";
-	cin.ignore();
+	cout << "Program: \t";
     getline(cin, program);
     
 	cout << "*********************************************************************" << endl;
 	
 	Course1Name:
 	cout << "Enter Course Name 1: \t";
-	cin.ignore();
     getline(cin, course1);
     
     Course1Grades:
@@ -68,7 +72,7 @@ int main(){
     
     Course2Name:
     cout << "Enter Course Name 2: \t";
-	cin.ignore();
+    cin.ignore();
     getline(cin, course2);
     
     Course2Grades:
@@ -84,7 +88,7 @@ int main(){
     
     Course3Name:
     cout << "Enter Course Name 3: \t";
-	cin.ignore();
+    cin.ignore();
     getline(cin, course3);
     
     Course3Grades:
@@ -100,7 +104,7 @@ int main(){
     
     Course4Name:
     cout << "Enter Course Name 4: \t";
-	cin.ignore();
+    cin.ignore();
     getline(cin, course4);
     
     Course4Grades:
@@ -119,5 +123,20 @@ int main(){
     	Sleep(1000);
     	cout << ".";
 }
+	system("cls");
+	
+	avg1 = subjA1 + subjA2 + subjA3 + subjA4 / 4;
+	avg2 = subjB1 + subjB2 + subjB3 + subjB4 / 4;
+	avg3 = subjC1 + subjC2 + subjC3 + subjC4 / 4;
+	avg4 = subjD1 + subjD2 + subjD3 + subjD4 / 4;
+
+	cout << "*********************************************************************" << endl;
+	cout << "Student Name: \t" << fname << endl; 
+	cout << "Program: \t" << program << endl;
+	cout << "*********************************************************************" << endl;
+	cout << "Course Name \t\t\t\t" << "Term 1 \t\t" << "Term 2 \t\t" << "Term 3 \t\t" << "Term 4 \t\t" << "Average \t" << "Remarks \t" << endl;
+	cout << course1 << setw(25) << "\t" << subjA1 << "\t" << subjA2 << "\t" << subjA3 << "\t" << subjA4 << "\t" << avg1;
+	cin.ignore();
+	getline(cin, course1);
 	return 0;
 }
